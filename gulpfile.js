be0a21b82482fb79a1e3084c88e169d2d1c190db
Steve Troetti <SteveXCIV@gmail.com>
2017-01-16 16:03:42 -0500

    Add basic gulp tasks

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+const gulp = require('gulp');
+const clean = require('gulp-clean');
+const ts = require('gulp-typescript');
+
+const project = ts.createProject('tsconfig.json');
+
+gulp.task('build', () => {
+    const output = project.src().pipe(project());
+    return output.js.pipe(gulp.dest(project.options.outDir));
+});
+
+gulp.task('clean', () => {
+    return gulp.src(project.options.outDir).pipe(clean());
+});
