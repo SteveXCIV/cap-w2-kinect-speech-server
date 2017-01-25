8ee66efd61fce65eaedf102c90f4aa8ca3bdc798
Steve Troetti <SteveXCIV@gmail.com>
2017-01-24 21:04:40 -0500

    Add database intialization to startup script

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import { install } from 'source-map-support';
+import mongoose from 'mongoose';
 import App from './app';
 import SessionRouter from './routers/session-router';
 import SessionService from './services/session-service';
 
 // Sets up debugging via source maps
 install();
+
+// Start up database -- replace this with environment vars to auth in prod
+mongoose.connect("mongodb://localhost:27017/cap-w2");
 
 let sessionService = new SessionService();
 
