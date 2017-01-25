43f92141574d4f24eeb6ea364f91a50140926828
Steve Troetti <SteveXCIV@gmail.com>
2017-01-24 19:40:35 -0500

    Add basic session data model

---
+++ src/models/session-model.js
@@ -0,0 +1,71 @@
+import mongoose from 'mongoose';
+
+const VALIDATION_ERROR_INVALID_ENUM = 'ValidationError ({PATH}): `{VALUE}` is not valid.'
+const VALIDATION_ERROR_MISSING_REQUIRED = 'ValidationError ({PATH}): Missing required value.'
+
+const JointType = {
+    values: [
+        'AnkleLeft',
+        'AnkleRight',
+        'ElbowLeft',
+        'ElbowRight',
+        'FootLeft',
+        'FootRight',
+        'HandLeft',
+        'HandRight',
+        'HandTipLeft',
+        'HandTipRight',
+        'Head',
+        'HipLeft',
+        'HipRight',
+        'KneeLeft',
+        'KneeRight',
+        'Neck',
+        'ShoulderLeft',
+        'ShoulderRight',
+        'SpineBase',
+        'SpineMid',
+        'SpineShoulder',
+        'ThumbLeft',
+        'ThumbRight'
+        'WristLeft',
+        'WristRight',
+    ],
+    message: VALIDATION_ERROR_INVALID_ENUM
+};
+
+const RequiredNumber = {
+    type: Number,
+    required: [ true, VALIDATION_ERROR_MISSING_REQUIRED ]
+};
+
+const Joint = {
+    JointType: {
+        type: String,
+        enum: JointType,
+        required: [ true, VALIDATION_ERROR_MISSING_REQUIRED ]
+    },
+    X: RequiredNumber,
+    Y: RequiredNumber,
+    Z: RequiredNumber
+};
+
+const Snapshot = {
+    Joints: {
+        type: [ Joint ],
+        required: [ true, VALIDATION_ERROR_MISSING_REQUIRED ]
+    },
+    Time: {
+        type: Date,
+        required: [ true, VALIDATION_ERROR_MISSING_REQUIRED ]
+    }
+};
+
+const Session = mongoose.Schema({
+    Snapshots: {
+        type: Snapshot,
+        required: [ true, VALIDATION_ERROR_MISSING_REQUIRED ]
+    }
+});
+
+export default mongoose.model('Session', Session);
