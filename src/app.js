1847befac8639affe7ca517dca86eb0a7263af5b
Steve Troetti <SteveXCIV@gmail.com>
2017-01-30 19:09:14 -0500

    Add public routing for App

--- src/app.js
+++ src/app.js
@@ -29,6 +29,8 @@ export default class {
         // Set up the middleware for JSON request/responses
         this.app.use(bodyParser.json());
         this.app.use(bodyParser.urlencoded({ extended: false }));
+
+        this.app.use(express.static(__dirname + '/public'));
     }
 
     setupRoutes() {
