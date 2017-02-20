53ba12f00c4d2a86cdd44244fed58edb07d5f5b7
Steve Troetti <SteveXCIV@gmail.com>
2017-02-20 13:56:51 -0500

    Change session schema to match new format

--- src/models/session-model.js
+++ src/models/session-model.js
@@ -1,13 +1,24 @@
 import errors from './error-messages';
 import mongoose from 'mongoose';
+import RequiredDate from './common';
+import Calibration from './calibration-model';
+import Trial from './trial-model';
+import PATIENT_NAME from './account-model';
 
-const Session = mongoose.Schema({
-    BodySnapshots: {
-        type: [ BodySnapshot ],
+const SessionSchema = mongoose.Schema({
+    CalibrationData: {
+        type: Calibration,
         required: [ true, errors.VALIDATION_ERROR_MISSING_REQUIRED ]
     },
-    AudioSnapshots: {
-        type: [ AudioSnapshot ],
+    EndTime: RequiredDate,
+    Patient: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: PATIENT_NAME,
+        required: [ true, errors.VALIDATION_ERROR_MISSING_REQUIRED ]
+    },
+    StartTime: RequiredDate,
+    Trials: {
+        type: [ Trial ],
         required: [ true, errors.VALIDATION_ERROR_MISSING_REQUIRED ]
     }
 },
@@ -16,4 +27,4 @@ const Session = mongoose.Schema({
 });
 
 export const SESSION_NAME = 'Session';
-export default mongoose.model(SESSION_NAME, Session);
+export default mongoose.model(SESSION_NAME, SessionSchema);
