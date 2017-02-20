bba771b315fa7ff8e24b6a7326312f2e6c4ed29d
Steve Troetti <SteveXCIV@gmail.com>
2017-02-20 12:03:59 -0500

    Add Objective schemas/models

---
+++ src/models/objective-model.js
@@ -0,0 +1,52 @@
+import errors from './error-messages';
+import mongoose from 'mongoose';
+
+const options = {
+    discriminatorKey: 'kind',
+    versionKey: false
+};
+
+const RequiredDate = {
+    type: Date,
+    required: [ true, erorrs.VALIDATION_ERROR_MISSING_REQUIRED ]
+}
+
+const ObjectiveSchema = mongoose.Schema({
+    AudioSnapshots: {
+        type: [ AudioSnapshot ],
+        required: [ true, errors.VALIDATION_ERROR_MISSING_REQUIRED ]
+    },
+    BodySnapshots: {
+        type: [ BodySnapshot ],
+        required: [ true, errors.VALIDATION_ERROR_MISSING_REQUIRED ]
+    },
+    EndTime: RequiredDate,
+    StartTime: RequiredDate
+}, options);
+
+const ObjectiveModel = mongoose.model('Objective', ObjectiveSchema);
+
+const DescribeObjectiveSchema = mongoose.Schema({
+    Distances: {
+        type: [ Distance2Snapshot ],
+        required: [ true, errors.VALIDATION_ERROR_MISSING_REQUIRED ]
+    }
+}, options);
+
+const LocateObjectiveSchema = mongoose.Schema({
+    ActivationTime: RequiredDate,
+    Distances: {
+        type: [ DistanceSnapshot ],
+        required: [ true, errors.VALIDATION_ERROR_MISSING_REQUIRED ]
+    }
+}, options);
+
+export const DescribeObjective = ObjectiveModel.discriminator(
+    'DescribeObjective',
+    DescribeObjectiveSchema
+);
+
+export const LocateObjective = ObjectiveModel.discriminator(
+    'LocateObjective',
+    LocateObjectiveSchema
+);
