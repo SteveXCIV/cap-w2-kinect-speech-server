fc6978e2282f58a0af866dace3c33afdc7698aa8
Steve Troetti <SteveXCIV@gmail.com>
2017-03-01 13:11:02 -0500

    Begin re-adding unit tests for Sessions

--- test/session-model.test.js
+++ test/session-model.test.js
@@ -9,5 +9,39 @@ import mongoose from 'mongoose';
 mongoose.Promise = global.Promise;
 
 describe('session-model tests', function() {
-    
+    const time = Date.now();
+    const calibration = {
+        EndTime: time,
+        StartTime: time
+    };
+    const id = mongoose.Types.ObjectId();
+    const locateObjective = {
+        AudioSnapshots: [{ Time: time }],
+        BodySnapshots: [{
+            Joints: [{ JointType: 'ElbowLeft' }],
+            Time: time
+        }],
+        EndTime: time,
+        StartTime: time,
+        kind: 'LocateObjective'
+    };
+    const trial = {
+        EndTime: time,
+        Objectives: [ locateObjective ],
+        StartTime: time
+    };
+
+    it('should be invalid without CalibrationData', done => {
+        let s = new Session({
+            EndTime: time,
+            Patient: id,
+            StartTime: time,
+            Trials: [ trial ]
+        });
+
+        s.validate(err => {
+            expect(err.errors.CalibrationData).to.exist;
+            done();
+        });
+    });
 });
