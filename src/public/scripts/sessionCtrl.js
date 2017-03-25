2bba39d3ffe0cb80006ade7280d4c2c612338256
Pooja Jhaveri <jhaveripn@gmail.com>
2017-03-25 13:44:35 -0400

    Added hand to object distance plot for locate objective

--- src/public/scripts/sessionCtrl.js
+++ src/public/scripts/sessionCtrl.js
@@ -2,15 +2,16 @@ angular
 .module('ngCapstone')
 .controller('sessionCtrl', function($scope, sessionFactory) {
 	
-	$scope.sessionindex = 3;
+	$scope.sessionindex = 2;
 	$scope.righthand = [];
 	$scope.lefthand = [];
 	$scope.spinemid = [];
 	$scope.intensity = [];
 	$scope.range = [];
-	$scope.rangeintensity = [];
-	$scope.timelabels = [];
+	$scope.handtohandaudio = [];
+	$scope.handtohandaudiolabels = [];
 	$scope.precision = [];
+	$scope.precisionlabels = [];
 
 	sessionFactory.getData().then(function(data) {
 		$scope.sessiondata = data.data[$scope.sessionindex];
@@ -18,8 +19,14 @@ angular
 		$scope.end = new Date($scope.sessiondata.EndTime);
 		$scope.duration = msConverter($scope.end - $scope.start);
 		
+		$scope.calibration = $scope.sessiondata.CalibrationData;
+		$scope.radius = $scope.sessiondata.CalibrationData.Radius;
+		$scope.pointingtimer = $scope.sessiondata.CalibrationData.PointingZoneTimerSec;
+		$scope.reachright = $scope.sessiondata.CalibrationData.MaxReachRight;
+		$scope.reachleft = $scope.sessiondata.CalibrationData.MaxReachLeft;
+		$scope.audiothreshold = $scope.sessiondata.CalibrationData.AudioThreshold;
+
 		$scope.objectives = $scope.sessiondata.Trials[0].Objectives;
-		console.log('session index is ' + $scope.sessionindex);
 		for (var i = 0; i < $scope.objectives.length; i++) {
 			angular.forEach($scope.objectives[i], function(value, key) {
 				if (key === "kind" && value === "LocateObjective") {
@@ -28,11 +35,25 @@ angular
 					$scope.locateend = new Date($scope.objectives[i].EndTime);
 					$scope.reactiontime = msConverter($scope.activationtime - $scope.locatestart);
 					$scope.locatecompletiontime = msConverter($scope.locateend - $scope.locatestart);
+
 					//$scope.accuracy.push([{}]); //accuracy of hand(s) position while in activation time window vs time
-					//for (j = 0; j < $scope.objectives[i].BodySnapshots.length; j++) {
-						//$scope.precision.push([{x: $scope.BodySnapshots[j].Joints[3].X, y: $scope.BodySnapshots[j].Joints[3].Y, r: (($scope.BodySnapshots[j].Joints[3].Z)-1.2)*5}]);
-					//};
+					
 					$scope.bodysnapshots = $scope.objectives[i].BodySnapshots;
+
+					for (j = 0; j < $scope.objectives[i].Distances.length; j++) {
+						$scope.currenttime = new Date($scope.objectives[i].Distances[j].Time);
+						if ($scope.currenttime > $scope.activationtime && $scope.currenttime < $scope.locateend) {
+							$scope.precisionlabels.push(msConverter($scope.currenttime - new Date($scope.objectives[i].Distances[0].Time)));
+							$scope.precision.push($scope.objectives[i].Distances[j].Distance);
+						};
+					};
+					$scope.precision = [$scope.precision];
+					$scope.precisionseries = ['Hand to Object Distance'];
+
+					//console.log($scope.objectives[0].Distances);
+					//console.log($scope.precision);
+					//console.log($scope.precisionlabels);
+					
 					for (var s = 0; s < $scope.bodysnapshots.length; s++) { 
 						for (var j = 0; j < 15; j++) { 
 							angular.forEach($scope.bodysnapshots[s].Joints[j], function(value, key) {
@@ -60,15 +81,18 @@ angular
 					for (var j = 0; j < $scope.objectives[i].Distances.length; j++) {
 						$scope.range.push($scope.objectives[i].Distances[j].HandToHandDistance);
 						$scope.currenttime = new Date($scope.objectives[i].Distances[j].Time);
-						$scope.timelabels.push(msConverter($scope.currenttime - new Date($scope.objectives[i].Distances[0].Time)));
+						$scope.handtohandaudiolabels.push(msConverter($scope.currenttime - new Date($scope.objectives[i].Distances[0].Time)));
 					};
 					for (var j = 0; j < $scope.objectives[i].AudioSnapshots.length; j++) {
 						$scope.intensity.push($scope.objectives[i].AudioSnapshots[j].Intensity);
 					};
 					$scope.averageintensity = Average($scope.intensity);
-					$scope.rangeintensity = [$scope.range].concat([$scope.intensity]);
-					$scope.rangeintensityseries = ['Hand to Hand Distance', 'Audio Intensity'];
+					$scope.handtohandaudio = [$scope.range].concat([$scope.intensity]);
+					$scope.handtohandaudioseries = ['Hand to Hand Distance', 'Audio Intensity'];
 					//completion time and average audio intensity
+
+					//console.log($scope.handtohandaudio);
+					//console.log($scope.handtohandaudiolabels);
 
 				};
 			});
@@ -77,30 +101,47 @@ angular
 		console.log(error);
 	});
 
+$scope.precisionoverride = [
+{ 
+	yAxisID: 'y-axis-1', 
+		borderColor: "rgb(4, 141, 183)", //blue
+		pointBackgroundColor: "rgb(4, 141, 183)", //blue
 
-	function msConverter(ms) {
-		var min = Math.floor(ms / 60000);
-		var sec = ((ms % 60000) / 1000).toFixed(0);
-		return min + ":" + (sec < 10 ? '0' : '') + sec;
+		backgroundColor:"rgba(220,220,220,0)", //light grey
+		
+		pointBorderColor: "#fff", //white
+		pointHoverBorderColor: "rgba(220,220,220,0)", //light grey
+		pointHoverBackgroundColor: "rgb(4, 141, 183)" //blue
+		//scaleShowGridLines: false,
+		//pointDot: false,
+		//bezierCurve: false
 	}
-
-	function Average(array) {
-		var sum = 0;
-		for(var a = 0; a < array.length; a++){sum += array[a];}
-			var average = (sum / array.length); return average;
-	}
-
-
-	$scope.onClick = function (points, evt) {
-		console.log(points, evt);
+	];
+	$scope.precisionoptions = {
+		title: {
+			display: true,
+			text: 'Hand to Object Distance vs. Time'
+		},
+		scales: {
+			yAxes: [{
+				scaleLabel: {
+					display: true,
+					labelString: 'Hand to Hand Distance'
+				},
+				id: 'y-axis-1',
+				type: 'linear',
+				display: true,
+				position: 'left'
+			}]
+		}
 	};
 
-	$scope.rangeintensityoverride = [
+	$scope.handtohandaudiooverride = [
 	{ 
 		yAxisID: 'y-axis-1', 
 		borderColor: "rgb(4, 141, 183)", //blue
 		pointBackgroundColor: "rgb(4, 141, 183)", //blue
-		
+
 		backgroundColor:"rgba(220,220,220,0)", //light grey
 		
 		pointBorderColor: "#fff", //white
@@ -123,19 +164,7 @@ angular
 	}
 
 	];
-
-
-
-	$scope.datasetOverride1 = [
-	{
-		backgroundColor:"#FF6384"
-	},
-	{
-		hoverBackgroundColor: "#FF6384"
-	}
-	];
-
-	$scope.rangeintensityoptions = {
+	$scope.handtohandaudiooptions = {
 		title: {
 			display: true,
 			text: 'Hand to Hand Distance and Audio Intensity vs. Time'
@@ -165,7 +194,15 @@ angular
 	};
 
 
-	$scope.options2 = {
+	$scope.rangeoverride = [
+	{
+		backgroundColor:"#FF6384"
+	},
+	{
+		hoverBackgroundColor: "#FF6384"
+	}
+	];
+	$scope.rangeoptions = {
 		title: {
 			display: true,
 			text: 'Range of Motion'
@@ -194,31 +231,22 @@ angular
 		}
 	};
 
-
-
-
-
-	var newdata = {
-		labels : ["January","February","March","April","May","June","July"],
-		datasets : [
-		{
-			fillColor : "rgba(220,220,220,0.5)",
-			strokeColor : "rgba(220,220,220,1)",
-			pointColor : "rgba(220,220,220,1)",
-			pointStrokeColor : "#fff",
-			data : [65,59,90,81,56,55,40]
-		},
-		{
-			fillColor : "rgba(151,187,205,0.5)",
-			strokeColor : "rgba(151,187,205,1)",
-			pointColor : "rgba(151,187,205,1)",
-			pointStrokeColor : "#fff",
-			data : [28,48,40,19,96,27,100]
-		}
-		]
+	function msConverter(ms) {
+		var min = Math.floor(ms / 60000);
+		var sec = ((ms % 60000) / 1000).toFixed(0);
+		return min + ":" + (sec < 10 ? '0' : '') + sec;
 	}
 
-	$scope.myChart = newdata;
+	function Average(array) {
+		var sum = 0;
+		for(var a = 0; a < array.length; a++){sum += array[a];}
+			var average = (sum / array.length); return average;
+	}
+
+
+	$scope.onClick = function (points, evt) {
+		console.log(points, evt);
+	};
 
 
 });
