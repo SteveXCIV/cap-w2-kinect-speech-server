9c3367cc3757ae1cd8bed2b341e1ae73176cddf3
Steve Troetti <SteveXCIV@gmail.com>
2017-03-25 15:35:39 -0400

    Clean up login and register controllers

--- src/public/scripts/loginCtrl.js
+++ src/public/scripts/loginCtrl.js
@@ -1,23 +1,19 @@
 (function() {
-    angular.module('ngCapstone').controller('loginCtrl', [
-        '$scope',
-        '$rootScope',
-        '$location',
-        'accountService',
-        function($scope, $rootScope, $location, accountService) {
-            $scope.login = function() {
-                $scope.dataLoading = true;
-                console.log("trying to log in");
-                accountService.login($scope.email, $scope.password, function(response) {
-                    console.log(response);
-                    if (response.data) {
-                        $location.path('/physician');
-                    } else {
-                        $scope.error = response.message;
-                        $scope.dataLoading = false;
-                    }
-                });
-            };
-        }
-    ]);
+    function loginController($scope, $rootScope, $location, accountService) {
+        $scope.login = function() {
+            $scope.dataLoading = true;
+            console.log("trying to log in");
+            accountService.login($scope.email, $scope.password, function(response) {
+                console.log(response);
+                if (response.data) {
+                    $location.path('/physician');
+                } else {
+                    $scope.error = response.message;
+                    $scope.dataLoading = false;
+                }
+            });
+        };
+    }
+
+    angular.module('ngCapstone').controller('loginCtrl', loginController);
 })();
--- src/public/scripts/registerCtrl.js
+++ src/public/scripts/registerCtrl.js
@@ -1,22 +1,17 @@
 (function() {
-    angular.module('ngCapstone').controller('registerCtrl', [
-        '$scope',
-        '$rootScope',
-        '$location',
-        'accountService',
-        function($scope, $rootScope, $location, accountService) {
-            $scope.register = function() {
-                $scope.dataLoading = true;
-                console.log($scope);
-                registerService.register($scope.email, $scope.password, $scope.verifyPassword, $scope.firstName, $scope.lastName).then(function(response) {
+    function registerController($scope, $location, accountService) {
+        $scope.register = function() {
+            $scope.dataLoading = true;
+            accountService.register($scope.email, $scope.password, $scope.verifyPassword, $scope.firstName, $scope.lastName)
+                .then(function(response) {
                     $scope.dataLoading = false;
                     $location.url('/physician');
                 }, function(error) {
                     $scope.dataLoading = false;
-                    console.log(error);
                     $scope.error = error.data.message;
                 });
-            };
-        }
-    ]);
+        };
+    }
+
+    angular.module('ngCapstone').controller('registerCtrl', registerController);
 })();
