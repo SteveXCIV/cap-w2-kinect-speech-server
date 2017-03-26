f744b8c02bae5be302032772d4379fdf6d7af822
Steve Troetti <SteveXCIV@gmail.com>
2017-03-26 14:09:27 -0400

    Implement fancy new account menu

---
+++ src/public/scripts/navCtrl.js
@@ -0,0 +1,13 @@
+(function() {
+    function navController($scope, $location, accountService) {
+        $scope.getUserName = function() {
+            let user = accountService.getUser();
+            if (!user || !user.firstName || !user.lastName) {
+                return '';
+            }
+            return user.firstName + ' ' + user.lastName;
+        }
+    }
+
+    angular.module('ngCapstone').controller('navCtrl', navController);
+})();
